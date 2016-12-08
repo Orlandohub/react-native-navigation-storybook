43384a81da01dba378693d1d1f045b3646476828
Guy Carmeli <guyca@users.noreply.github.com>
2016-12-08 16:03:38 +0200

    Revert "adding detox" (#548)
    This reverts commit 5e2a4d087897c42670ba15603e79840a6f984a32.

--- ios/example/AppDelegate.m
+++ ios/example/AppDelegate.m
@@ -15,11 +15,8 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
   NSURL *jsCodeLocation;
-#ifdef DEBUG
   jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/index.ios.bundle?platform=ios&dev=true"];
-#else
-   jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
-#endif
+  // jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
 
 
   // **********************************************
