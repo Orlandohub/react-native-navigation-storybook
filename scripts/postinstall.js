3eb809931f1f2de63b12e4973cbe7238cec66333
Daniel Zlotin <danielzl@wix.com>
2016-12-14 17:44:56 +0200

    fixed example

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,4 +1,3 @@
 const cp = require('child_process');
 cp.execSync(`rm -rf node_modules/react-native-navigation/node_modules`);
 cp.execSync(`rm -rf node_modules/react-native-navigation/example `);
-cp.execSync(`rm -rf yarn.lock`);
--- src/screens/PushedScreen.js
+++ src/screens/PushedScreen.js
@@ -60,7 +60,8 @@ export default class PushedScreen extends Component {
   onResetToPress() {
     this.props.navigator.resetTo({
       title: "New Root",
-      screen: "example.ThirdTabScreen"
+      screen: "example.StyledScreen",
+      animated: true
     });
   }
 }
