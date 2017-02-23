feb32fffbe4fbced944c4336025b0f10a1447c07
Daniel Zlotin <danielzl@wix.com>
2017-02-23 15:07:47 +0200

    migrated ios to RN41.2, bugs not included

--- ios/example/AppDelegate.m
+++ ios/example/AppDelegate.m
@@ -8,7 +8,7 @@
 // IMPORTANT: if you're getting an Xcode error that RCCManager.h isn't found, you've probably ran "npm install"
 // with npm ver 2. You'll need to "npm install" with npm 3 (see https://github.com/wix/react-native-navigation/issues/1)
 
-#import "RCTRootView.h"
+#import <React/RCTRootView.h>
 
 @implementation AppDelegate
 
