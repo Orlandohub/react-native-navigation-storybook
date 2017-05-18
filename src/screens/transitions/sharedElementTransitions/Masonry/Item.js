cce5c57220f19d86ecc32acbcaeea27cb37ece96
Guy Carmeli <guyc@wix.com>
2017-05-18 17:43:05 +0300

    Fix masonry item alignment issue

--- src/screens/transitions/sharedElementTransitions/Masonry/Item.js
+++ src/screens/transitions/sharedElementTransitions/Masonry/Item.js
@@ -43,7 +43,6 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#ffffff',
-        alignItems: 'center',
         justifyContent: 'center',
     },
     image: {
