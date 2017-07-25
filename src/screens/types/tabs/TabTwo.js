931b1b65fec24dc7e2186ffd4e3043b7be3b0689
Guy Carmeli <guyc@wix.com>
2017-07-25 12:35:33 +0300

    Removed unused imports

--- src/screens/types/tabs/TabTwo.js
+++ src/screens/types/tabs/TabTwo.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {StyleSheet, View, Text, Dimensions, Button} from 'react-native';
+import {View, Text} from 'react-native';
 
 class TabOne extends React.Component {
 
