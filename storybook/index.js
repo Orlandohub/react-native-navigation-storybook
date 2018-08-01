880cc46d1077c7413996f596f1b7f310d5ec2b2e
Orlando Goncalves <orlando.goncalves@gmail.com>
2018-08-01 18:47:43 +0200

    Readme update

--- storybook/index.js
+++ storybook/index.js
@@ -1,3 +1,22 @@
-import StorybookUI from './storybook';
+import React, { Component } from 'react';
+import { AppRegistry } from 'react-native';
+import { Navigation } from "react-native-navigation";
+import { getStorybookUI, configure } from '@storybook/react-native';
+
+// import stories
+configure(() => {
+  require('./stories');
+}, module);
+
+const StorybookUI = getStorybookUI({ port: 7007, host: "localhost" });
+
+Navigation.registerComponent("storybook.UI", () => StorybookUI);
+
+Navigation.startSingleScreenApp({
+  screen: {
+    screen: "storybook.UI",
+    title: "Storybook",
+  },
+});
 
 export default StorybookUI;
